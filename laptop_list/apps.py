39961eab117461efa87f2edb71b2984c659edb89
Naveen <21a91f0052@aec.edu.in>
2022-12-24 10:14:49 +0530

    first

---
+++ laptop_list/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LaptopListConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'laptop_list'
---
+++ laptop_list/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def Home(request):
+    return HttpResponse('status-ok')
